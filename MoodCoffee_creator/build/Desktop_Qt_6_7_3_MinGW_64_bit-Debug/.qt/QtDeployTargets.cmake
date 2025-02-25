893a21b0e6df0ea0388f8abe41c400e29b87413f
GLARTEmbedded <2987983T@student.gla.ac.uk>
2025-02-26 06:33:41 +0800

    Add my_folder with code

---
+++ MoodCoffee_creator/build/Desktop_Qt_6_7_3_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_untitled_FILE C:/Users/86186/Desktop/MOODCAFE_UI/MoodCoffee_creator/build/Desktop_Qt_6_7_3_MinGW_64_bit-Debug/untitled.exe)
+set(__QT_DEPLOY_TARGET_untitled_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_untitled_RUNTIME_DLLS D:/QT/6.7.3/mingw_64/bin/Qt6Widgets.dll;D:/QT/6.7.3/mingw_64/bin/Qt6Gui.dll;D:/QT/6.7.3/mingw_64/bin/Qt6Core.dll)
