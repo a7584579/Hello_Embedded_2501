ef38f648d929afa4837306188238ccf4eca72af9
Jiaqi Fan <jiaqifan@glaroam2-153-42.wireless.gla.ac.uk>
2025-03-05 16:47:29 +0000

    2nd version

--- untitled/header/actuator_module_handle.h
+++ untitled/header/actuator_module_handle.h
@@ -1,35 +1,27 @@
 #ifndef ACTUATOR_MODULE_HANDLE_H
 #define ACTUATOR_MODULE_HANDLE_H
 
-#include <QObject>
 #include <QThread>
-#include <pigpio.h>
-#include <QAudioOutput>
-#include <QMediaPlayer>
+#include <QProcess>
+#include <array>
+#include <QString>
 
-class Actuator_Module_Handle : public QObject
-{
-    Q_OBJECT
+// Actuator_Module_Handle提供执行器的控制接口
+class Actuator_Module_Handle {
 public:
-    explicit Actuator_Module_Handle(QObject *parent = nullptr);
+    Actuator_Module_Handle();
     ~Actuator_Module_Handle();
 
-    void startBumpThread(const int durations[4]); // bump start thread
-    void startAudioThread(const QString &filePath); // audio start thread
+    // 启动水泵控制线程，参数为四个水泵各自开启的时长（毫秒）
+    void startPumpThread(const std::array<int, 4>& durations);
+    // 启动音频播放线程，参数为要播放的MP4文件的那个路径
+    void startSpeakerThread(const QString &filePath);
 
 private:
-    QThread bumpThread;
-    QThread audioThread;
-    // bump control
-    void controlBump(const int durations[4]);
-
-    // audio control
-    void playSound(const QString &filePath);
-
-    // GPIO
-    const int bumpPins[4] = {14, 15, 23, 24}; // bump used gpio
-
-signals:
+    // 指向水泵和音频线程的指针
+    QThread *pumpThread;
+    QThread *speakerThread;
 };
 
 #endif // ACTUATOR_MODULE_HANDLE_H
+
