33fccf1b6076c3a5888e970c817ac92d77a2dc2b
Aria933 <WYY115933@163.com>
2025-04-22 04:50:22 +0800

    Clean up old source files and update CMakeLists.txt and main.cpp

--- untitled/main.cpp
+++ untitled/main.cpp
@@ -1,11 +1,20 @@
 #include "mainwindow.h"
-
+#include "coffee_daemon.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
     MainWindow w;
     w.show();
+
+    CoffeeDaemon daemon;
+    daemon.startDaemon();
+
+    QObject::connect(&a, &QCoreApplication::aboutToQuit, [&daemon]() {
+        daemon.stopDaemon();
+    });
+
     return a.exec();
 }
